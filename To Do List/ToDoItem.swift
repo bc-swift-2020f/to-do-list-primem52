25876e31b0166f98dec8e7ee2e05e37557ca6d31
Morgan Prime <primem@bc.edu>
2020-09-28 11:30:28 -0400

    5.7

---
+++ To Do List/ToDoItem.swift
@@ -0,0 +1,16 @@
+//
+//  ToDoItem.swift
+//  To Do List
+//
+//  Created by Morgan Prime on 9/28/20.
+//  Copyright © 2020 Morgan Prime. All rights reserved.
+//
+
+import Foundation
+
+struct ToDoItem: Codable {
+     var name: String
+     var date: Date
+     var notes: String
+ }
+ 
--- To Do List/ToDoListViewController.swift
+++ To Do List/ToDoListViewController.swift
@@ -8,58 +8,122 @@
 
 import UIKit
 
+
 class ToDoListViewController: UIViewController {
+ 
+    var toDoItems: [ToDoItem] = []
+    
     @IBOutlet weak var tableView: UITableView!
     
-    var toDoArray = ["he","she","me","ye"]
+    @IBOutlet weak var addBarButton: UIBarButtonItem!
+  
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         tableView.delegate = self
         tableView.dataSource = self
+        
+        loadData()
+    }
+    
+    
+    func loadData() {
+        let directoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let documentURL = directoryURL.appendingPathComponent("todos").appendingPathExtension("json")
+        
+        guard let data = try? Data(contentsOf: documentURL) else {return}
+        
+        let jsonDecoder = JSONDecoder()
+        do {
+            toDoItems = try jsonDecoder.decode(Array<ToDoItem>.self, from: data)
+            tableView.reloadData()
+        }
+        catch{
+            print("Error couldnt load data \(error.localizedDescription)")
+        }
+    }
+    
+    func saveData() {
+        let directoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let documentURL = directoryURL.appendingPathComponent("todos").appendingPathExtension("json")
+        let jsonEncoder = JSONEncoder()
+        let data = try? jsonEncoder.encode(toDoItems)
+        do {
+            try data?.write(to: documentURL, options: .noFileProtection)
+        }
+        catch{
+            print("Error couldnt save data \(error.localizedDescription)")
+        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "ShowDetail"{
             let destination = segue.destination as! ToDoDetailTableViewController
             let selectedIndexPath = tableView.indexPathForSelectedRow!
-            destination.toDoItem = toDoArray[selectedIndexPath.row]
+            destination.toDoItem = toDoItems[selectedIndexPath.row]
         }
         else {
             if let selectedIndexPath = tableView.indexPathForSelectedRow {
                 tableView.deselectRow(at: selectedIndexPath, animated: true) // maybe need to put in brackets
             }
-        }
+        } 
     }
     @IBAction func unwindFromDetail(segue: UIStoryboardSegue){
         let source = segue.source as! ToDoDetailTableViewController
         if let selectedIndexPath = tableView.indexPathForSelectedRow{
-            toDoArray[selectedIndexPath.row] = source.toDoItem
+            toDoItems[selectedIndexPath.row] = source.toDoItem
             tableView.reloadRows(at: [selectedIndexPath], with: .automatic)
         }
         else {
-            let newIndexPath = IndexPath(row: toDoArray.count, section: 0)
-            toDoArray.append(source.toDoItem)
+            let newIndexPath = IndexPath(row: toDoItems.count, section: 0)
+            toDoItems.append(source.toDoItem)
             tableView.insertRows(at: [newIndexPath], with: .bottom)
             tableView.scrollToRow(at: newIndexPath, at: .bottom, animated: true)
         }
+        saveData()
     }
 
-
+    @IBAction func editButtonPressed(_ sender: UIBarButtonItem) {
+        if tableView.isEditing {
+            tableView.setEditing(false, animated: true)
+            sender.title = "Edit"
+            addBarButton.isEnabled = true
+            
+        }
+        else {
+            tableView.setEditing(true, animated: true)
+            sender.title = "Done"
+            addBarButton.isEnabled = false
+        }
+    }
+    
 }
 
 extension ToDoListViewController: UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        print("numberOfrowsinSection was just called returning \(toDoArray.count)")
-        return toDoArray.count
+        return toDoItems.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = toDoArray[indexPath.row]
+        cell.textLabel?.text = toDoItems[indexPath.row].name
         return cell
     }
     
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            toDoItems.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            saveData()
+            
+        }
+    }
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let itemToMove = toDoItems[sourceIndexPath.row]
+        toDoItems.remove(at: sourceIndexPath.row)
+        toDoItems.insert(itemToMove, at: destinationIndexPath.row)
+        saveData()
+    }
     
 }
 
